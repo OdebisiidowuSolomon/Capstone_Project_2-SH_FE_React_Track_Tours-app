9651086898ce618ec64d8d6c00864333500a5894
Odebisi Solomon idowu <Odebisiidowu09@gmail.com>
2021-11-29 14:55:52 +0100

    pushing starters files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
-function App() {
-  const [tours, setTours] = useState([]);
-  useEffect(() => {
-    axios.get(url).then((res) => setTours(res.data));
-  }, []);
-  return (
-    <main>
-      <div className="sectio">
-        {tours.length > 0 ? <Tours tours={tours} /> : <Loading />}
-      </div>
-    </main>
-  );
-}
+function App() {}
 
 export default App;
