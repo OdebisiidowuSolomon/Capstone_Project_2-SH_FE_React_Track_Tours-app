1dfbdf067761c8880cae817f7d2bafa971d0ba64
Odebisi Solomon idowu <Odebisiidowu09@gmail.com>
2021-11-29 13:52:26 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [tours, setTours] = useState([]);
+  useEffect(() => {
+    axios.get(url).then((res) => setTours(res.data));
+  }, []);
+  return (
+    <main>
+      <div className="sectio">
+        {tours.length > 0 ? <Tours tours={tours} /> : <Loading />}
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Tours.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+export default function Tours({ tours: tourx }) {
+  const [shortenText, setShortenText] = useState(true);
+  const [tours, setTours] = useState(tourx);
+
+  const handleText = (text) => {
+    return shortenText ? `${text.slice(0, 200)} ...` : `${text.slice(0)}`;
+  };
+
+  const handleTextButton = () => (shortenText ? "Show more" : "Show Less");
+
+  const handleDelete = (id) => {
+    setTours((prev) => prev.filter((tour) => tour.id !== id));
+  };
+
+  return (
+    <>
+      {tours.length > 0 ? (
+        <>
+          <div className="title">
+            <h2>Our Tours</h2>
+            <div className="underline"></div>
+          </div>
+          {tours.map((tour) => (
+            <div className="single-tour" key={tour.id}>
+              <img src={tour.image} alt="tourImage" className="img" />
+              <div className="tour-content">
+                <div className="tour-info">
+                  <h4>{tour.name}</h4>
+                  <div className="tour-price">
+                    <h4>$1,233</h4>
+                  </div>
+                </div>
+                <div className="tour-body">
+                  {handleText(tour.info)}
+                  <button onClick={() => setShortenText((prev) => !prev)}>
+                    {handleTextButton()}
+                  </button>
+                </div>
+              </div>
+              <footer>
+                <button
+                  className="delete-btn"
+                  onClick={handleDelete.bind(this, tour.id)}
+                >
+                  Not Interested
+                </button>
+              </footer>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className="title">
+          <h2>No More Tours Remaining</h2>
+          <button className="btn" onClick={() => setTours(tourx)}>
+            Refresh
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
