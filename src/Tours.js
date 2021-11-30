409c47f549db6f37cd423c92004d0eea4b123fbc
alvinpeter9 <alvinpeter9@gmail.com>
2021-11-30 13:52:00 +0100

    arranged the tour component

--- src/Tours.js
+++ src/Tours.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 export default function Tours() {
-  return <div></div>;
+  const [tour, settour] = useState([]);
+
+  return(
+    <>
+    <h2>Our Tours</h2> 
+
+    {tour.map(tour=>(
+      <div className=''>
+      <img src={tour.image} alt={tour.name} />
+      <div className=''>
+      <div className=''>
+      <h4> {tour.name} </h4>
+      <div className=''><h4>
+      {tour.price} </h4></div>
+      <div className=''>
+      {tour.info} 
+      </div>
+      </div>
+      </div>
+      <footer>
+        <button className=''>Not Interested</button>
+      </footer>
+      </div>
+      )
+    )}
+    </>
+  );
 }
