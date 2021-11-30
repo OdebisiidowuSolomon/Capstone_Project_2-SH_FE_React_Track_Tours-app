0cbbe5f73d1042644683a5e66448389a7943f41f
AyoOlu1 <oluwayemivictor15@gmail.com>
2021-11-30 21:30:05 +0100

    styled the h1 element

--- src/Tours.js
+++ src/Tours.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 
-
 export default function Tours() {
-  const [tour, settour] = useState([]);
+	const [tour, setTour] = useState([]);
 
-  return(
-    <>
-    <h2>Our Tours</h2> 
+	return (
+		<>
+			<h1>
+				Our Tours
+				<hr />
+			</h1>
 
-    {tour.map(tour=>(
-      <div className=''>
-      <img src={tour.image} alt={tour.name} />
-      <div className=''>
-      <div className=''>
-      <h4> {tour.name} </h4>
-      <div className=''><h4>
-      {tour.price} </h4></div>
-      <div className=''>
-      {tour.info} 
-      </div>
-      </div>
-      </div>
-      <footer>
-        <button className=''>Not Interested</button>
-      </footer>
-      </div>
-      )
-    )}
-    </>
-  );
+			{tour.map((tour) => (
+				<div className="">
+					<img src={tour.image} alt={tour.name} />
+					<div className="">
+						<div className="">
+							<h4> {tour.name} </h4>
+							<div className="">
+								<h4>{tour.price} </h4>
+							</div>
+							<div className="">{tour.info}</div>
+						</div>
+					</div>
+					<footer>
+						<button className="">Not Interested</button>
+					</footer>
+				</div>
+			))}
+		</>
+	);
 }
