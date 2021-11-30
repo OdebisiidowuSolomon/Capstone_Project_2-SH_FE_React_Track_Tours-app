0f7c6325bdea758417bc0de33b10ad574c16ed14
Baribor Saturday <saturdaybaribor@gmail.com>
2021-11-30 12:52:40 +0100

    finishing touch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 import Loading from "./Loading";
 import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
-function App() {}
+function App() {
+  const [tours, setTours] = useState(null);
+  const [loading, setLoading] = useState(true); //kinda a dumb value to trigger useEffect
+  
+  useEffect(()=>{
+    if(!tours || tours.length===0){
+    fetch(url)
+    .then(res=>res.json())
+   .then(data=>{
+     setTours(data);
+     setLoading(false);
+   })
+   .catch((err)=>alert(err.message));
+   } 
+  },[loading]);
+  
+  const removeTour = (id)=>{
+    let currentTours = tours.filter((tour)=>tour.id!==id);
+    setTours(currentTours);
+    
+  }
+  
+  return(
+    <main>
+    {
+      (tours && !loading)? <>
+      
+       {
+       tours.length>0?
+       <>
+        <h2 className="title">Our Tours</h2>
+        <p className="underline"></p>
+       </>:
+        
+       <>
+       
+        <h2 className='title'>No more Tours remaining</h2>
+        <div style={{textAlign:'center'}}><span className='btn' onClick={()=>setLoading(true)}>Refresh</span></div>
+        
+       </>
+       
+       } 
+      
+      <Tours tours={tours} handleRemove={removeTour} />
+      </>
+      :
+      <Loading />
+    }
+    </main>
+   
+    );
+}
 
 export default App;
--- src/Tours.js
+++ src/Tours.js
@@ -1,5 +1,55 @@
 import React from "react";
+import { useState } from 'react';
 
-export default function Tours() {
-  return <div></div>;
+export default function Tours({tours, handleRemove}) {
+  
+  
+  const Tour = ({props})=>{
+    const max_length = 200;
+    let {id, image, info, price,name} = props;
+    const [currentInfo, setCurrentInfo] = useState(info.substr(0,max_length)+'...');
+    
+    const showMoreLess = (evt)=>{
+      let btnState = evt.target.innerText.toLowerCase();
+      if(btnState.indexOf('less')!==-1){
+        evt.target.innerText = 'show more';
+        setCurrentInfo(info.substr(0,max_length));
+      }else{
+        evt.target.innerText = 'show less';
+        setCurrentInfo(info);
+      }
+    }
+    
+    return(
+     <div className="single-tour">
+
+      <img src={image} alt="sample image" />
+      <div className="tour-content">
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        <div>
+          <p>
+            <span>
+            {currentInfo}
+            </span>
+          <button onClick={showMoreLess}>show more</button></p>
+        </div>
+        <footer>
+          <div className="delete-btn" onClick={()=>{handleRemove(id)} }>
+            Not interested
+          </div>
+        </footer>
+      </div>
+    </div>);
+  }
+  
+  return (
+    <>
+    {
+      tours.map((t)=><Tour key={t.id}props={t} />) 
+    } 
+    </>
+    );
 }
